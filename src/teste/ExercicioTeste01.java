84b44ee330ea96462fee360a5b1b64bbbf3e9e3e
Elthon <eltonsilva.mecatronic@gmail.com>
2022-06-21 17:34:57 -0300

    Exercicio 01 Teste comcluido

--- src/teste/ExercicioTeste01.java
+++ src/teste/ExercicioTeste01.java
@@ -1,4 +1,33 @@
 package teste;
 
-public class exercicioTeste01 {
+import dominio.Exercicio01;
+
+public class ExercicioTeste01 {
+    public static void main(String[] args) {
+        Exercicio01 estudante01 = new Exercicio01();
+        Exercicio01 estudante02 = new Exercicio01();
+
+        estudante01.matricula = 903840;
+        estudante01.nome = "Elton";
+        estudante01.nota1 = 7.00f;
+        estudante01.nota2 = 7.00f;
+        estudante01.notaTrabalho = 8.00f;
+
+
+        estudante02.matricula = 904663;
+        estudante02.nome = "João";
+        estudante02.nota1 = 3.00f;
+        estudante02.nota2 = 9.00f;
+        estudante02.notaTrabalho = 4.00f;
+
+        estudante01.calculaMedia();
+
+
+        System.out.println();
+
+        estudante02.calculaMedia();
+
+
+
+    }
 }
